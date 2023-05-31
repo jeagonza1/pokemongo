9850fc5815594883d19ce2fb0b2fc47e2993da24
Jean <jfga1508@gmail.com>
2023-05-31 14:11:58 -0500

    changed api port

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const services = require('./services');
 const bodyParser = require('body-parser');
 const debug = require('debug')('express-react:server');
 const http = require('http');
+const port = '3000';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('port', 3001);
+app.set('port', port);
 app.use('/api', index);
 
 const server = http.createServer(app);
 
-server.listen(3001);
+server.listen(port);
 server.on('listening', onListening);
 
 function onListening() {
